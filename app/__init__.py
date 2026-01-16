6dc2372bad524d407c6aa0594de74a91511148aa
jlmontielc <jorgelmc05@gmail.com>
2026-01-16 13:48:50 -0400

    .

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+        'mysql://root:WPfIdZtSOMtimSnSDNhKnKImtkpgZuSi@turntable.proxy.rlwy.net:56208/railway'
+    )
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
     @app.route("/")
     def home():
         return "Sistema de Gestión de Shalas funcionando"
-    
+
     return app
